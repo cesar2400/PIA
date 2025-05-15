c29a65b9179b73841f8419b99551f81e045da78d
cesar2400 <alejandro.alanisgrrr@uanl.edu.mx>
2025-05-15 13:11:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
             return
     else:
         # Sin internet, cargamos lat/lon desde archivo local (como prueba.json)
-        print("⚠️ No hay conexión a internet. Usando datos locales de respaldo...")
+        print("No hay conexión a internet. Usando datos locales de respaldo...")
         datos_locales = mo.cargar_json_local()
         if datos_locales is None:
             print("❌ No se pudo cargar el archivo local.")
@@ -29,20 +29,20 @@ def main():
     
 
     if mo.hay_internet():
-        print("✅ Conexión detectada. Consultando PVGIS...")
+        print("Conexión detectada. Consultando PVGIS...")
         datos = mo.obtener_datos_pvgis(lat, lon, potencia_kw, perdidas)
     else:
-        print("⚠️ Sin conexión. Cargando nuevamente los datos locales...")
+        print("Sin conexión. Cargando nuevamente los datos locales...")
         datos = mo.cargar_json_local()
 
     if datos:
         energia_mensual = mo.extraer_energia_mensual(datos)
-        print("📊 Energía mensual estimada (kWh):")
+        print(" Energía mensual estimada (kWh):")
         for mes, energia in energia_mensual.items():
             print(f"{mes.capitalize()}: {energia:.2f}")
         mo.graficar_energia_mensual(energia_mensual)
     else:
-        print("❌ No se pudieron obtener los datos del sistema.")
+        print("No se pudieron obtener los datos del sistema.")
 
     pe.exportar_a_excel(energia_mensual, ubi, nombre_archivo="informe_solar.xlsx")
 
